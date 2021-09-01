09908bc64eb56c6f8256de8c151e9e5754bbbfc6
Ana Ruiz <ruizana393@gmail.com>
2021-08-31 20:02:47 -0400

    Tiny App ,Login completed

--- express_server.js
+++ express_server.js
@@ -6,11 +6,14 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  "b2xVn2": "http://www.lighthouselabs.ca", 
   "9sm5xK": "http://www.google.com"
 };
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+var cookieParser = require('cookie-parser')
+app.use(cookieParser())
+
 
 
 app.get("/", (req, res) => {
@@ -27,28 +30,35 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.get("/urls",(req, res) => {
-  const templateVars ={urls: urlDatabase };
+  const templateVars ={
+    urls: urlDatabase ,
+  username: req.cookies["username"],
+};
   res.render("urls_index", templateVars);
 });
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]}
+  res.render("urls_new", templateVars);
 });
 app.get("/u/:shortURL", (req, res) => {
-  const longURL =  urlDatabase[req.params.shortURL];
+const longURL =  urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
 app.get("/urls/:shortURL",(req, res) => {
   if(urlDatabase[req.params.shortURL]){
   const longURL = urlDatabase[req.params.shortURL];  
-  const templateVars = {shortURL: req.params.shortURL, longURL: longURL};
+  const templateVars = {shortURL: req.params.shortURL, longURL: longURL, username: req.cookies["username"]};
   res.render("urls_show", templateVars);
   }
   else{
     res.redirect("/urls");
   }
 });
-
+function generateRandomString(number) {
+  return randomstring.generate(number);
+  }
+  
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   let randomString = generateRandomString(6);
@@ -58,6 +68,31 @@ app.post("/urls", (req, res) => {
   //res.send(randomString);         // Respond with 'Ok' (we will replace this)
 });
 
-function generateRandomString(number) {
-return randomstring.generate(number);
-}
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
+app.post("/urls/:shortURL/edit", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL
+  res.redirect("/urls");
+});
+
+app.get("/urls/:shortURL/edit", (req, res) => {
+  console.log(req.params);
+  res.redirect("/urls/:shortURL");
+});
+
+app.post("/login", (req, res) => {
+  res.cookie("username",req.body.username)
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username")
+  res.redirect("/urls");
+});
+
+
+
